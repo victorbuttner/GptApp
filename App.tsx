5a1460a1f37dd15163e2769d78f9486b41904ea6
Victor Buttner <victorbuttner@gmail.com>
2024-03-21 00:52:16 -0300

    feat: add newRelic

--- App.tsx
+++ App.tsx
@@ -1,16 +1,56 @@
 import React, {useState, useEffect} from 'react';
 import {GiftedChat} from 'react-native-gifted-chat';
 import axios from 'axios';
+import NewRelic from 'newrelic-react-native-agent';
 import {version} from './package.json';
 import {
   AppRegistry,
   Button,
+  Platform,
   StyleSheet,
   Text,
   TextInput,
   View,
 } from 'react-native';
+let appToken;
+console.log(Platform.OS);
+if (Platform.OS === 'ios') {
+  appToken = 'AAe423c8d68cb73e34d1cb7fafde32026e739a4f50-NRMA';
+} else {
+  appToken = 'AA7bb0104f7f41bb9b01b59ae73efa934a2a3812c1-NRMA';
+}
+const agentConfiguration = {
+  //Android Specific
+  // Optional:Enable or disable collection of event data.
+  analyticsEventEnabled: true,
+  // Optional:Enable or disable crash reporting.
+  crashReportingEnabled: true,
+  // Optional:Enable or disable interaction tracing. Trace instrumentation still occurs, but no traces are harvested. This will disable default and custom interactions.
+  interactionTracingEnabled: true,
+  // Optional:Enable or disable reporting successful HTTP requests to the MobileRequest event type.
+  networkRequestEnabled: true,
+  // Optional:Enable or disable reporting network and HTTP request errors to the MobileRequestError event type.
+  networkErrorRequestEnabled: true,
+  // Optional:Enable or disable capture of HTTP response bodies for HTTP error traces, and MobileRequestError events.
+  httpRequestBodyCaptureEnabled: true,
+  // Optional:Enable or disable agent logging.
+  loggingEnabled: true,
+  // Optional:Specifies the log level. Omit this field for the default log level.
+  // Options include: ERROR (least verbose), WARNING, INFO, VERBOSE, AUDIT (most verbose).
+  logLevel: NewRelic.LogLevel.INFO,
+  // iOS Specific
+  // Optional:Enable/Disable automatic instrumentation of WebViews
+  webViewInstrumentation: true,
+  // Optional:Set a specific collector address for sending data. Omit this field for default address.
+  // collectorAddress: "",
+  // Optional:Set a specific crash collector address for sending crashes. Omit this field for default address.
+  // crashCollectorAddress: "",
+  // Optional:Enable or disable reporting data using different endpoints for US government clients
+  // fedRampEnabled: false
+};
 
+NewRelic.startAgent(appToken, agentConfiguration);
+NewRelic.setJSAppVersion(version);
 AppRegistry.registerComponent('ChatGPTApp', () => App);
 
 const styles = StyleSheet.create({
@@ -28,14 +68,19 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   error: {
+    // Adicione esta nova definição de estilo
     color: 'red',
     textAlign: 'center',
     padding: 10,
   },
+  // Outros estilos podem ser adicionados aqui
 });
-
 const WelcomeScreen = ({onSubmitUsername}) => {
   const [username, setUsername] = useState('');
+
+  const simulateCrash = () => {
+    NewRelic.crashNow('This is a test crash');
+  };
 
   return (
     <View style={styles.container}>
@@ -46,12 +91,13 @@ const WelcomeScreen = ({onSubmitUsername}) => {
         onChangeText={setUsername}
       />
       <Button title="Enter Chat" onPress={() => onSubmitUsername(username)} />
+      <Button title="Simulate Crash" onPress={simulateCrash} />
     </View>
   );
 };
 
 const App = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null); // Adiciona esta linha para declarar o estado do usuário
   const [errorMessage, setErrorMessage] = useState('');
   const [messages, setMessages] = useState([]);
 
@@ -75,6 +121,13 @@ const App = () => {
     setMessages(GiftedChat.append(messages, newMessages));
     const message = newMessages[0].text;
 
+    NewRelic.setAttribute('username', user ? user.name : 'undefined');
+
+    NewRelic.recordBreadcrumb('MessageSent', {
+      message: message,
+      username: user ? user.name : 'undefined',
+    });
+
     console.log('message', {
       source: 'UserMessage',
       text: message,
@@ -82,17 +135,19 @@ const App = () => {
     });
 
     if (message.toLowerCase().includes('erro')) {
+      // Lança uma exceção que você pode capturar e tratar
       setErrorMessage('Um erro foi detectado em sua mensagem.');
       throw new Error('Uma mensagem de erro foi detectada.');
     }
     if (message.toLowerCase().includes('exception')) {
+      // Lança uma exceção que você pode capturar e tratar
       throw new Error('Uma mensagem de erro foi detectada.');
     }
     try {
       const response = await axios.post(
-        'https://api.openai.com/v1/chat/completions',
+        'https://api.openai.com/v1/chat/completions', // Endpoint da API de Chat Completions
         {
-          model: 'gpt-3.5-turbo',
+          model: 'gpt-3.5-turbo', // Especifique o modelo que você deseja usar
           messages: [
             {
               role: 'user',
@@ -104,11 +159,16 @@ const App = () => {
           headers: {
             'Content-Type': 'application/json',
             Authorization:
-              'Bearer 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',
+              'Bearer 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', // Sua chave de API
           },
         },
       );
       const replies = response.data.choices[0].message.content.trim();
+
+      NewRelic.recordCustomEvent('OpenAIResponse', 'message', {
+        message: replies,
+        username: user ? user.name : 'undefined',
+      });
 
       console.log('message', {
         source: 'OpenAIResponse',
@@ -128,15 +188,18 @@ const App = () => {
         }),
       );
     } catch (error) {
+      NewRelic.recordError(new Error(String(error)));
       console.error(error);
     }
   };
-
   const onSubmitUsername = username => {
+    // Quando um username é submetido, atualize o estado do usuário
     setUser({
       _id: 1,
       name: username,
     });
+
+    NewRelic.setUserId(username);
   };
 
   if (!user) {
